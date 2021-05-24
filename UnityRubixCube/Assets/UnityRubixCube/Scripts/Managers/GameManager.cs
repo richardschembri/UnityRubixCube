78c940145d84c1bc8abcb03c25c6992ca6093732
Richard Schembri <richard@localhost.localdomain>
2021-05-25 00:06:58 +0900

    Additional UI elements

--- UnityRubixCube/Assets/UnityRubixCube/Scripts/Managers/GameManager.cs
+++ UnityRubixCube/Assets/UnityRubixCube/Scripts/Managers/GameManager.cs
@@ -83,7 +83,7 @@ namespace UnityRubixCube.Managers{
         // Start is called before the first frame update
         void Start()
         {
-            
+           _mainMenu.OpenPopup(); 
         }
 
         // Update is called once per frame
